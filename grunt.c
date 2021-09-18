45e890804c53ab4f195f445c9cbbfd97ff3ab67d
LyfeMaster <64602623+LyfeMaster@users.noreply.github.com>
2021-09-18 11:00:46 -0400

    assignment 2

---
+++ grunt.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+    printf("1");
+    printf("\n");
+    printf("1.2");
+
+    unsigned int a = 504;
+    unsigned int b = -24;
+    double c = 2 / 10;
+
+    short t = 9999999;
+
+    return 12;
+}
+
+int square(int n){
+    return n*n;
+}
